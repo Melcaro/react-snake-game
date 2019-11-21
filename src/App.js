028041031c1ac91f86aa5397e93e4fd99cb3e8fa
melanie caro <melanie.caro@stackerine.com>
2019-11-21 10:51:58 +0100

    create a form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { SnakeGame } from './SnakeGame';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.el = null;
+  }
+
+  componentDidMount() {
+    this.el.focus();
+  }
+  setTextInput = el => (this.el = el);
+
+  render() {
+    return (
+      <div className="App">
+        <h1>FORM</h1>
+        <form>
+          <div className="lastNameInfos">
+            <label for="lastName">Enter your last name : </label>
+            <input type="text" name="lastName" id="lastName" />
+          </div>
+          <br />
+          <div className="firstNameInfos">
+            <label for="firstName">Enter your first name : </label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              ref={this.setTextInput}
+            />
+          </div>
+          <br />
+          <div className="genderInfos">
+            <label for="gender" name="gender" id="gender">
+              Choose your gender :
+            </label>
+            <br />
+            <input id="maleGender" type="checkbox" />
+            Male
+            <br />
+            <input id="femaleGender" type="checkbox" />
+            Female
+            <br />
+            <input id="unspecifiedGender" type="checkbox" />
+            Unspecified
+            <br />
+          </div>
+          <br />
+          <div className="ageInfos">
+            <label for="age">Enter your age : </label>
+            <input type="text" name="age" id="age" />
+          </div>
+        </form>
+        <h1>SNAKE GAME</h1>
+        <SnakeGame />
+      </div>
+    );
+  }
 }
 
 export default App;
